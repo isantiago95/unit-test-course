067d48e90ae73417e7ae6ed5da3615f87b2e12a8
Israel Santiago <israel.santiago4359@gmail.com>
2023-07-27 18:23:07 -0700

    finish section 34

--- JS-practical-guide/basic-testing-frontend/src/util/numbers.test.js
+++ JS-practical-guide/basic-testing-frontend/src/util/numbers.test.js
@@ -1,22 +1,31 @@
-import { it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { transformToNumber } from './numbers';
 
-it('should transform a string number to a number type', () => {
-    const input = '1';
+describe('transformToNumber()', () => {
+    it('should transform a string number to a number type', () => {
+        const input = '1';
 
-    const result = transformToNumber(input);
+        const result = transformToNumber(input);
 
-    expect(result).toBeTypeOf('number');
-    expect(result).toBe(+input);
+        expect(result).toBeTypeOf('number');
+        expect(result).toBe(+input);
+    });
+
+    it('should yield NaN for non-transformable values', () => {
+        const input = 'invalid';
+        const input2 = {};
+
+        const result = transformToNumber(input);
+        const result2 = transformToNumber(input2);
+
+        expect(result).toBeNaN();
+        expect(result2).toBeNaN();
+    });
 });
 
-it('should yield NaN for non-transformable values', () => {
-    const input = 'invalid';
-    const input2 = {};
+describe('cleanNumbers()', () => {
+    it('should return an array of nnumbers if an array of string number is provided', () => {
+        const numberValues = ['1', '2'];
 
-    const result = transformToNumber(input);
-    const result2 = transformToNumber(input2);
-
-    expect(result).toBeNaN();
-    expect(result2).toBeNaN();
+    });
 });
